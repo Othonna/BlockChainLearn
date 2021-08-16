2dfe8572c1a148cc458390b67bb768f935fc0406
Janick Othonna <janick.deneux@gmail.com>
2021-08-16 17:57:47 +0200

    endOf

--- BcLearn.js
+++ BcLearn.js
@@ -14,3 +14,51 @@ class Block {
     }
 
 }
+
+class Blockchain {
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+    }
+
+    createGenesisBlock() {
+        return new Block(0, "16/08/2021", "Genesis block", "0");
+    } 
+
+    getLastedBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock) {
+       newBlock.previousHash = this.getLastedBlock().hash;
+       newBlock.hash = newBlock.calculateHash();
+       this.chain.push(newBlock);
+    }    
+
+    isChainValid() {
+        for(let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if (currentBlock.hash !== currentBlock.calculateHash()) {
+                return false;
+            }
+
+            if (currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+let OthonnaCoin = new Blockchain();
+OthonnaCoin.addBlock(new Block(1, "20/08/2021", { amount: 4}));
+OthonnaCoin.addBlock(new Block(2, "27/08/2021", { amount: 10}));
+
+//console.log(JSON.stringify(OthonnaCoin, null, 4));
+//console.log('Is blockchain valid ? ' + OthonnaCoin.isChainValid());
+
+//OthonnaCoin.chain[1].data = { amount: 100};
+
+//console.log('Is blockchain valid ? ' + OthonnaCoin.isChainValid());
