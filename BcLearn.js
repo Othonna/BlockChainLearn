49aa4ac4aca25970829acf2b0ff261b9eb9c5b06
Janick Othonna <janick.deneux@gmail.com>
2021-08-17 10:50:28 +0200

    proof-of-work

--- BcLearn.js
+++ BcLearn.js
@@ -7,13 +7,22 @@ class Block {
         this.data = data;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
+        this.nonce = 0;
     }
    
     calculateHash(){
         return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
     }
-
+    
+    mineBlock(difficulty) {
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+        console/log("Block moned: " + this.hash);
+    }
 }
+
 
 class Blockchain {
     constructor() {
@@ -30,7 +39,7 @@ class Blockchain {
 
     addBlock(newBlock) {
        newBlock.previousHash = this.getLastedBlock().hash;
-       newBlock.hash = newBlock.calculateHash();
+       newBlock.mineBlock();
        this.chain.push(newBlock);
     }    
 
